fb788b55ccc0b761137ff731200a29889701f9c9
LaChapeliere <emmamarion.barme@orange.fr>
2016-10-06 19:42:44 +0200

    Very basic function that reads the inputs so they can be printed, with doc. The player only search the waterhole they're in and don't move

---
+++ markovMove.R
@@ -0,0 +1,17 @@
+
+markovMove <- function(moveInfo, readings, positions, edges, probs) {
+  #moveInfo = list(moves = c('the two moves to make'), mem = list('any info we want to store')
+  #readings = c('salinity reading from Croc', 'phosphate reading from Croc', 'nitrogen reading from Croc')
+  #positions = c('position of tourist 1', 'position of tourist 2', 'position of player')
+  #edges = matrix('edges paths between waterholes, first column is one extremity of the edge, second column is the other extremity, every row is one edge')
+  #probs = list(salinity = matrix('mean and standard deviation of readings for salinity, each row is a waterhole, first column is mean, second column is standard deviation'), phosphate = matrix('mean and standard deviation of readings for phosphate, each row is a waterhole, first column is mean, second column is standard deviation'), nitrogen = matrix('mean and standard deviation of readings for nitrogen, each row is a waterhole, first column is mean, second column is standard deviation'))
+
+  print(moveInfo)
+  print(readings)
+  print(positions)
+  print(edges)
+  print(probs)
+  
+  moveInfo[['moves']] = c(0, 0)
+  return(moveInfo)
+}
