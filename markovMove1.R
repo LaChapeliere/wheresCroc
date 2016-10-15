504e8109c4722194c9f9aca10f6b807ddf9344f7
michaelws92 <michael.wsaputra@gmail.com>
2016-10-16 00:56:36 +0700

    Markovmove1
    My function is markovMove1

--- markovMove1.R
+++ markovMove1.R
@@ -38,7 +38,9 @@ computeProbabilities <- function(observations, previousProbabilities, probs, nei
   #neighbors = 'definition of the network of waterhole by list of lists of neighbors
   
   #If this is the first turn
-  if (sum(previousProbabilities) == 0) {
+  print("prev Prob")
+  print(sum(previousProbabilities))
+  if (sum(previousProbabilities) == 0 || !is.nan(sum(previousProbabilities))){
     previousProbabilities = vector(mode="double", length=40)
     possibleWaterholes = 0
     for (waterhole in 1:40) {
@@ -86,235 +88,244 @@ computeProbabilities <- function(observations, previousProbabilities, probs, nei
   return(probas)
 }
 
+
 makeMove <- function(probas, positions, edges) {
   #probas: list of possibilities for each hole
   #positions: croc, backpacker 1, bacpacker 2, ranger
   #edges: relation of one hole to another
   
   #find max value of probability
-  maxProb = 0
-  maxProb2 = 0
-  tempData = 100
-  flag = 0
-  # for (i in 1:40) {
-  #   if(maxProb < probas[i])
-  #     maxProb = i
-  # }
-  
-  ##################################
-  ###########My Trying COde#########
-  ##################################
-  options=getOptions(positions[3],edges)
-  # print("Position")
-  # print(positions[3])
-  # print(options[3])
-  # print("Length of Options")
-  # print(length(options))
-  
-  # for(i in 1:length(options))
-  # {
-  #   print(options[i])
-  #   print(probas[options[i]])
-  # }
-
-  
-  # pCrocs = 0
-  # tempData = 100
-  # if(positions[2] < 0 || positions[3] < 0 || positions[1] < 0 || !is.na(positions[1]) || !is.na(positions[2]) || !is.na(positions[3]) )
-  # {
-  #   for (i in 1:40) {
-  #     if(pCrocs < probas[i])
-  #       pCrocs = i
-  #   }
-  #   for(i in 1:length(options))
-  #   {
-  # 
-  #     tempCal = pCrocs - options[i]
-  #     if(abs(tempCal) < tempData)
-  #       tempData = tempCal
-  #   }
-  #   maxProb = tempData
-  # }
-  # else
-  # {
-  ######Move for the First
-  for(i in 1:length(options))
+  if(all(!is.nan(probas)))
   {
-    if(probas[options[i]] != 0 && !is.nan(probas[options[i]]))
-    {
-      if(maxProb < probas[options[i]])
-        maxProb = options[i]
-      flag = flag + 1
-    }
-  }
-  if(flag == 0)
-  {
-    highProbabs = 0
-    for(j in 1:40)
-    {
-      if(highProbabs < probas[j] && j != positions[3])
-        highProbabs = j
-    }
-    # print("Options flag = 0")
-    # print(options)
-    tempI = 0
-    for(i in 1:length(options))
-    {
-      if(options[i] != positions[3])
-      {
-         tempValue = abs(highProbabs-options[i])
-         # print("Flag = 0")
-         # print(tempValue)
-         # print("Temp Data")
-         # print(tempData)
-         if(tempValue < tempData)
-         {
-           tempData = tempValue
-           tempI = i
-         }
-      }
-    }
-    maxProb = options[tempI]
+      maxProb = 0
+      maxProb2 = 0
+      tempData = 100
+      flag = 0
+      # for (i in 1:40) {
+      #   if(maxProb < probas[i])
+      #     maxProb = i
+      # }
+      
+      ##################################
+      ###########My Trying COde#########
+      ##################################
+      options=getOptions(positions[3],edges)
+      # print("Position")
+      # print(positions[3])
+      # print(options[3])
+      # print("Length of Options")
+      # print(length(options))
+      
+      # for(i in 1:length(options))
+      # {
+      #   print(options[i])
+      #   print(probas[options[i]])
+      # }
     
-  }
-  # }
-  # print(probas)
-  print("MaxProb")
-  print(maxProb)
-  
-  ########Move for the second
-  flag2 = 0
-  tempData = 100
-  optionsStep2 = getOptions(maxProb,edges)
-  for(i in 1:length(optionsStep2))
-  {
-    if(probas[optionsStep2[i]] != 0 && !is.nan(optionsStep2[options[i]]))
-    {
-      if(maxProb2 < probas[optionsStep2[i]])
-        maxProb2 = optionsStep2[i]
-      flag2 = flag2 + 1
-    }
-  }
-  if(flag2 == 0)
-  {
-    highProbabs = 0
-    for(j in 1:40)
-    {
-      if(highProbabs < probas[j] && j != positions[3])
-        highProbabs = j
-    }
-    print("Options flag2 = 0")
-    print(optionsStep2)
-    tempI = 0
-    for(i in 1:length(optionsStep2))
-    {
-      if(optionsStep2[i] != maxProb)
+      
+      # pCrocs = 0
+      # tempData = 100
+      # if(positions[2] < 0 || positions[3] < 0 || positions[1] < 0 || !is.na(positions[1]) || !is.na(positions[2]) || !is.na(positions[3]) )
+      # {
+      #   for (i in 1:40) {
+      #     if(pCrocs < probas[i])
+      #       pCrocs = i
+      #   }
+      #   for(i in 1:length(options))
+      #   {
+      # 
+      #     tempCal = pCrocs - options[i]
+      #     if(abs(tempCal) < tempData)
+      #       tempData = tempCal
+      #   }
+      #   maxProb = tempData
+      # }
+      # else
+      # {
+      ######Move for the First
+      for(i in 1:length(options))
       {
-        tempValue = abs(highProbabs-optionsStep2[i])
-        print("Flag2 = 0")
-        print(tempValue)
-        print("Temp Data2")
-        print(tempData)
-        if(tempValue < tempData)
+        if(probas[options[i]] != 0 && !is.nan(probas[options[i]]))
         {
-          tempData = tempValue
-          tempI = i
+          if(maxProb < probas[options[i]])
+            maxProb = options[i]
+          flag = flag + 1
         }
       }
-    }
-    print("Optimal Options2")
-    print(optionsStep2[tempI])
-    maxProb2 = optionsStep2[tempI]
-    
-  }
-  # }
-  print(probas)
-  print("MaxProb2")
-  print(maxProb2)
-  
-  
-  # optionStep2=getOptions(maxProb,edges)
-  # print(maxProb)
-  # print("Options2")
-  # for(i in 1:length(optionStep2))
-  # {
-  #   print(optionStep2[i])
-  #   print(probas[optionStep2[i]])
-  # }
-  # pCrocs = 0
-  # tempData = 100
-  # 
-  # print(positions[1])
-  # print(positions[3])
-  # if(positions[2] < 0 || positions[3] < 0 || positions[1] < 0 || !is.na(positions[1]) || !is.na(positions[2]) || !is.na(positions[3]))
-  # {
-  #   for (i in 1:40) {
-  #     if(pCrocs < probas[i])
-  #       pCrocs = i
-  #   }
-  #   for(i in 1:length(optionStep2))
-  #   {
-  # 
-  #     tempCal = pCrocs - optionStep2[i]
-  #     if(abs(tempCal) < tempData)
-  #       tempData = tempCal
-  #   }
-  #   maxProb2 = tempData
-  # }
-  # else
-  # {
-  #   for(i in length(optionStep2))
-  #     if(maxProb2 < probas[optionStep2[i]])
-  #       maxProb2 = optionStep2[i]
-  # }
-  # 
-  # print("Test1")
-  # print(maxProb)
-  # print("Test111")
-  # print(optionStep2)
-  # 
-  # 
-  # 
-  # print("Test2")
-  # print(maxProb2)
-  
- 
-  # 
-  # tempData = c(maxProb,maxProb2)
-  # print(tmppath)vec = as.vector(t(edges))
-  # megraph = make_graph(vec, directed=FALSE)
-  # #tmp <- graph.bfs(megraph, root=positions[3], neimode='all', order=TRUE, father=TRUE)
-  # 
-  # 
-  # tmppath = shortest_paths(megraph, positions[3], to = maxProb, mode ='all',
-  #                weights = NULL, output = 'both',
-  #                predecessors = FALSE, inbound.edges = FALSE)       
-  # 
-  # print("Test")
-  # print("Test")
-  # print(options)
-  # print(options[1])
-  #print(edges)
-  #print(edges(1,1))
-  
-  #find path to maxProb
-  flagCheck <<- flagCheck + 1
-  print("Flag")
-  print(flagCheck)
-  if(flagCheck %% 2 == 0)
-  {
-    move = c(maxProb,0)
-    probas[positions[3]] = 0
+      if(flag == 0)
+      {
+        highProbabs = 0
+        for(j in 1:40)
+        {
+          if(highProbabs < probas[j] && j != positions[3])
+            highProbabs = j
+        }
+        # print("Options flag = 0")
+        # print(options)
+        tempI = 0
+        for(i in 1:length(options))
+        {
+          if(options[i] != positions[3])
+          {
+             tempValue = abs(highProbabs-options[i])
+             # print("Flag = 0")
+             # print(tempValue)
+             # print("Temp Data")
+             # print(tempData)
+             if(tempValue < tempData)
+             {
+               tempData = tempValue
+               tempI = i
+             }
+          }
+        }
+        maxProb = options[tempI]
+        
+      }
+      # }
+      # print(probas)
+      # print("MaxProb")
+      # print(maxProb)
+      
+      ########Move for the second
+      flag2 = 0
+      tempData = 100
+      optionsStep2 = getOptions(maxProb,edges)
+      for(i in 1:length(optionsStep2))
+      {
+        if(probas[optionsStep2[i]] != 0 && !is.nan(optionsStep2[options[i]]))
+        {
+          if(maxProb2 < probas[optionsStep2[i]])
+            maxProb2 = optionsStep2[i]
+          flag2 = flag2 + 1
+        }
+      }
+      if(flag2 == 0)
+      {
+        highProbabs = 0
+        for(j in 1:40)
+        {
+          if(highProbabs < probas[j] && j != positions[3])
+            highProbabs = j
+        }
+        # print("Options flag2 = 0")
+        # print(optionsStep2)
+        tempI = 0
+        for(i in 1:length(optionsStep2))
+        {
+          if(optionsStep2[i] != maxProb)
+          {
+            tempValue = abs(highProbabs-optionsStep2[i])
+            # print("Flag2 = 0")
+            # print(tempValue)
+            # print("Temp Data2")
+            # print(tempData)
+            if(tempValue < tempData)
+            {
+              tempData = tempValue
+              tempI = i
+            }
+          }
+        }
+        # print("Optimal Options2")
+        # print(optionsStep2[tempI])
+        maxProb2 = optionsStep2[tempI]
+        
+      }
+      # }
+      # print(probas)
+      # print("MaxProb2")
+      # print(maxProb2)
+      
+      
+      # optionStep2=getOptions(maxProb,edges)
+      # print(maxProb)
+      # print("Options2")
+      # for(i in 1:length(optionStep2))
+      # {
+      #   print(optionStep2[i])
+      #   print(probas[optionStep2[i]])
+      # }
+      # pCrocs = 0
+      # tempData = 100
+      # 
+      # print(positions[1])
+      # print(positions[3])
+      # if(positions[2] < 0 || positions[3] < 0 || positions[1] < 0 || !is.na(positions[1]) || !is.na(positions[2]) || !is.na(positions[3]))
+      # {
+      #   for (i in 1:40) {
+      #     if(pCrocs < probas[i])
+      #       pCrocs = i
+      #   }
+      #   for(i in 1:length(optionStep2))
+      #   {
+      # 
+      #     tempCal = pCrocs - optionStep2[i]
+      #     if(abs(tempCal) < tempData)
+      #       tempData = tempCal
+      #   }
+      #   maxProb2 = tempData
+      # }
+      # else
+      # {
+      #   for(i in length(optionStep2))
+      #     if(maxProb2 < probas[optionStep2[i]])
+      #       maxProb2 = optionStep2[i]
+      # }
+      # 
+      # print("Test1")
+      # print(maxProb)
+      # print("Test111")
+      # print(optionStep2)
+      # 
+      # 
+      # 
+      # print("Test2")
+      # print(maxProb2)
+      
+     
+      # 
+      # tempData = c(maxProb,maxProb2)
+      # print(tmppath)vec = as.vector(t(edges))
+      # megraph = make_graph(vec, directed=FALSE)
+      # #tmp <- graph.bfs(megraph, root=positions[3], neimode='all', order=TRUE, father=TRUE)
+      # 
+      # 
+      # tmppath = shortest_paths(megraph, positions[3], to = maxProb, mode ='all',
+      #                weights = NULL, output = 'both',
+      #                predecessors = FALSE, inbound.edges = FALSE)       
+      # 
+      # print("Test")
+      # print("Test")
+      # print(options)
+      # print(options[1])
+      #print(edges)
+      #print(edges(1,1))
+      
+      #find path to maxProb
+      flagCheck <<- flagCheck + 1
+      # print("Flag")
+      # print(flagCheck)
+      if(flagCheck %% 2 == 0)
+      {
+        move = c(maxProb,0)
+        probas[positions[3]] = 0
+      }
+      else
+      {
+        move = c(maxProb,maxProb2)
+        # probas[maxProb]= 0;
+        # probas[maxProb2]=0;
+      }
+      # probas[maxProb2]=0;
+      # print("Move")
+      # print(move)
   }
   else
   {
-    move = c(maxProb,maxProb2)
-    # probas[maxProb]= 0;
-    # probas[maxProb2]=0;
+    print("Test11")
+    move = c(0,0)
   }
-  # probas[maxProb2]=0;
-  # print("Move")
-  # print(move)
   
   return(list("move"=move,"probas"=probas))
 }
@@ -376,12 +387,11 @@ markovMove1 <- function(moveInfo, readings, positions, edges, probs) {
   ################################
   
   #To Michael and Asa: Don't forget to ajust the probas if you check a waterhole, to pass the vector to the next turn
-  
-  # print("Flag")
-  # print(flagCheck)
+  #print(probas)
+  #print(probas[2])
   moveResult = makeMove(probas, positions, edges)
   moveInfo[['moves']] = moveResult$move
-  # moveInfo[['moves']] = c(0, 0)
+  #moveInfo[['moves']] = c(0, 0)
   moveInfo[['mem']][["previousProbabilities"]] = moveResult$probas
   return(moveInfo)
 }
