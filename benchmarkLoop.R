e440b4ef00200925e4add1c6942a9a849bba7449
LaChapeliere <emmamarion.barme@orange.fr>
2016-10-15 18:02:39 +0200

    Correcting benchmark loop

--- benchmarkLoop.R
+++ benchmarkLoop.R
@@ -1,7 +1,7 @@
 benchmarkLoop <- function(n) {
   results = vector(mode="integer", length=n)
   for (i in 1:n) {
-    results[n] = runWheresCroc(markovMove, pause = 0.1)
+    results[i] = runWheresCroc(markovMove, pause = 0.1)
   }
   
   print("Results:")
